e0cda041ccbab83600e1fc5bc6b744333f17bcdd
laserimouto <laser.imouto>
2023-05-14 19:17:02 +1000

    Sleep to prevent MIDI input reading from blocking forever under Windows

--- Python/midifilter.py
+++ Python/midifilter.py
@@ -1,8 +1,10 @@
 import mido
 import sys
+import time
 
 if len(sys.argv) < 3:
-    print("USAGE: midifilter <MIDI IN> <MIDI OUT>")
+    print("MIDI Inputs: ", mido.get_input_names())
+    print("MIDI Outputs: ", mido.get_output_names())
     exit()
 
 MIDI_IN_NAME = sys.argv[1]
@@ -11,21 +13,24 @@ FADER_CH_LSB = [32, 51, 63]
 FADER_CH_MSB = [0, 19, 31]
 
 last_value_sent = {}
-for channel in range(4):
+for channel in range(7):
     for control in FADER_CH_MSB:
         last_value_sent[(channel, control)] = -1
 
 with mido.open_input(MIDI_IN_NAME) as midi_in, mido.open_output(MIDI_OUT_NAME) as midi_out:
     print("Running...")
-    for msg in midi_in:
-        if msg.is_cc() and msg.control in FADER_CH_LSB:
-            continue
-
-        if msg.is_cc() and msg.control in FADER_CH_MSB:
-            if msg.value == last_value_sent[(msg.channel, msg.control)]:
+    while True:
+        for msg in midi_in.iter_pending():
+            if msg.is_cc() and msg.control in FADER_CH_LSB:
                 continue
-            else:
-                last_value_sent[(msg.channel, msg.control)] = msg.value
 
-        print(msg)
-        midi_out.send(msg)
+            if msg.is_cc() and msg.control in FADER_CH_MSB:
+                if msg.value == last_value_sent[(msg.channel, msg.control)]:
+                    continue
+                else:
+                    last_value_sent[(msg.channel, msg.control)] = msg.value
+
+            print(msg)
+            midi_out.send(msg)
+        
+        time.sleep(1 / 1000)
